3f8ff95bc1df629ae3bac7380f45d342c7e1442e
VladislavsDz <49658792+VladislavsDz@users.noreply.github.com>
2020-05-06 19:45:02 +0000

    javascript
    add some lines and fix mistake in brackets

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-$(document).ready()) =>{
-	$('#searchForm').on('sumbit' , (e) =>){
+$(document).ready(() => {
+	$('#searchForm').on('sumbit' , (e) => {
 		let searchText = $('#searchText').val();
-}
+		getMovies(searchText);
+		e.preventDefault();
+});
+});
