f6a5322c83e3d705899263efa488a78a1102ff15
VladislavsDz <49658792+VladislavsDz@users.noreply.github.com>
2020-05-06 19:48:23 +0000

    javascript function
    create function for searching in javascript and link where they searching the movies

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ $(document).ready(() => {
 		e.preventDefault();
 });
 });
+
+
+function getMovies(searchText){
+	axios.get('http://www.omdbapi.com?s='+searchText)
+	.then((response) =>{
+		console.log(response);
+	})
